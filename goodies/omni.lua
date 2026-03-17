b5848289a2694a9a0b3b39531a5b50780fa4d257
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-03-17 12:51:17 +0700

    Create omni.lua

---
+++ goodies/omni.lua
@@ -0,0 +1,49 @@
+-- UhhhhhhReanim/Modules/omni.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Omni"
+	m.Description = "500 years of constant human evolution"
+	m.Assets      = {"omni.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/omni.anim", "omni.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/jumper.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "how do i add distortionsoundeffect?", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("omni.mp3"), "omniman", 0.8, NumberRange.new(0, 39))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("omni.anim"))
+		animator.looped = false
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Update = function(dt: number, figure: Model)
+		animator:Step(GetOverrideDanceMusicTime())
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
