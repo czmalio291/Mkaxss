9845b0151eac2b8d63e549ebd43f854ba0857af3
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-03-17 12:34:19 +0700

    Create jumper.lua

---
+++ goodies/Jumper/jumper.lua
@@ -0,0 +1,45 @@
+-- UhhhhhhReanim/Modules/jumper.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Jumper"
+	m.Description = "ometry ash"
+	m.Assets      = {"jumper.anim", "jumper.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("jumper.mp3"), "gdjumper", 1, NumberRange.new(10.85, 600))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("jumper.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
