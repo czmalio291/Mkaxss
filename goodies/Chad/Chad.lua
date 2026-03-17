790d2f486e74f888298d9e599b61a25b5e3d194d
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-03-17 17:46:00 +0700

    Create Chad.lua

---
+++ goodies/Chad/Chad.lua
@@ -0,0 +1,49 @@
+-- UhhhhhhReanim/Modules/chad.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Chad"
+	m.Description = "can't outchad the chad"
+	m.Assets      = {"chad.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/chad.anim", "chad.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/chad.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, ":3 :3 :3 :3 :3 :3", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("chad.mp3"))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("chad.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+	
+	m.Update = function(dt: number, figure: Model)
+		animator:Step(GetOverrideDanceMusicTime())
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
