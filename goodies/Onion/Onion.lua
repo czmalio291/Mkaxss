7bd402fd246c9c47bc8be7fec8aded06bbeedec7
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-03-20 11:32:04 +0700

    Create Onion.lua

---
+++ goodies/Onion/Onion.lua
@@ -0,0 +1,49 @@
+-- UhhhhhhReanim/Modules/Onion.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Onion"
+	m.Description = "What if we say chocolate instead of onion!!!!!"
+	m.Assets      = {"Onion.anim", "Onion.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("Onion.mp3"))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Onion.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+  
+	m.Update = function(dt: number, figure: Model)
+		animator:Step(GetOverrideDanceMusicTime())
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
