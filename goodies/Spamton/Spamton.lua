ccaccfd8a2a135d7ad8b507d4140e53025f6be5b
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-03-20 19:47:55 +0700

    Create Spamton.lua

---
+++ goodies/Spamton/Spamton.lua
@@ -0,0 +1,45 @@
+-- UhhhhhhReanim/Modules/Spamton.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Spamton"
+	m.Description = "THAT'S RIGHT!! NOW'S YOUR CHANCE TO BE A [BIG SHOT]!!"
+	m.Assets      = {"Spamton.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/Spamton.anim", "Spamton.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/Spamton.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("Spamton.mp3"))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Spamton.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
