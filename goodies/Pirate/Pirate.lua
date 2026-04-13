bd84964718f94cc5ae6cd87307e3bba0b57189bc
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-04-13 07:25:15 +0700

    Update Pirate.lua

--- goodies/Pirate/Pirate.lua
+++ goodies/Pirate/Pirate.lua
@@ -8,7 +8,7 @@ table.insert(modules, function()
 	m.ModuleType  = "DANCE"
 	m.Name        = "Pirate"
 	m.Description = "the 1700s still hit hard"
-	m.Assets      = {"Pirate.anim@https://github.com/czmalio291/Mkaxss/blob/main/goodies/contents/Pirate.anim", "Pirate.mp3@https://github.com/czmalio291/Mkaxss/blob/main/goodies/contents/Pirate.mp3"}
+	m.Assets      = {"nil_Pirate.anim@https://github.com/czmalio291/Mkaxss/blob/main/goodies/contents/nil_Pirate.anim", "Pirate.mp3@https://github.com/czmalio291/Mkaxss/blob/main/goodies/contents/nil_Pirate.mp3"}
 
 	m.Config = function(parent)
 		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
@@ -21,12 +21,12 @@ table.insert(modules, function()
 	local start    = 0
 
 	m.Init = function(figure)
-		SetOverrideDanceMusic(AssetGetContentId("Pirate.mp3"), "1700s", 1, NumberRange.new(0, 600))
+		SetOverrideDanceMusic(AssetGetContentId("nil_Pirate.mp3")
 
 		start           = os.clock()
 		animator        = AnimLib.Animator.new()
 		animator.rig    = figure
-		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("Pirate.anim"))
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("nil_Pirate.anim"))
 		animator.looped = true
 		animator.speed  = 1
 	end
