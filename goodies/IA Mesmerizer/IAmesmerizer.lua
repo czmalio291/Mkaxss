3da0fc07225b7ab476c3f362f3f935cda12ab058
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-04-13 07:21:17 +0700

    Update IAmesmerizer.lua

--- goodies/IA Mesmerizer/IAmesmerizer.lua
+++ goodies/IA Mesmerizer/IAmesmerizer.lua
@@ -8,7 +8,7 @@ table.insert(modules, function()
 	m.ModuleType  = "DANCE"
 	m.Name        = "IA Mesmerizer"
 	m.Description = "Item asylum mesmerizer boohoo\nBasically a looped mesmerizer"
-	m.Assets      = {"IAmesmerizer.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/IAmesmerizer.anim", "IAmesmerizer.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/IAmesmerizer.mp3"}
+	m.Assets      = {"nil_IAmesmerizer.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/nil_IAmesmerizer.anim", "nil_IAmesmerizer.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/nil_IAmesmerizer.mp3"}
 
 	m.Config = function(parent)
 		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
@@ -21,12 +21,12 @@ table.insert(modules, function()
 	local start    = 0
 
 	m.Init = function(figure)
-		SetOverrideDanceMusic(AssetGetContentId("IAmesmerizer.mp3"), "Item Asylum mesmerizer", 0.8, NumberRange.new(0, 600))
+		SetOverrideDanceMusic(AssetGetContentId("nil_IAmesmerizer.mp3"), "Item Asylum mesmerizer", 0.9, NumberRange.new(0, 600))
 
 		start           = os.clock()
 		animator        = AnimLib.Animator.new()
 		animator.rig    = figure
-		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("IAmesmerizer.anim"))
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("nil_IAmesmerizer.anim"))
 		animator.looped = true
 		animator.speed  = 1
 	end
