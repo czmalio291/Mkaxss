83075c08024e13f8d27e8ca7a869032b2819a95e
czmalio291 <102205558+czmalio291@users.noreply.github.com>
2026-04-28 23:27:39 +0700

    Create Chtio.lua

---
+++ goodies/Chito/Chtio.lua
@@ -0,0 +1,45 @@
+-- UhhhhhhReanim/Modules/Chito.lua
+
+local modules = {}
+
+table.insert(modules, function()
+	local m = {}
+
+	m.ModuleType  = "DANCE"
+	m.Name        = "Chito"
+	m.Description = "More one night"
+	m.Assets      = {"nil_Chito.anim@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/nil_Chito.anim", "Onenight.mp3@https://github.com/czmalio291/Mkaxss/raw/refs/heads/main/goodies/contents/Onenight.mp3"}
+
+	m.Config = function(parent)
+		Util_CreateText(parent, "No settings.", 14, Enum.TextXAlignment.Center)
+	end
+
+	m.SaveConfig = function() return {} end
+	m.LoadConfig  = function(save) end
+
+	local animator = nil
+	local start    = 0
+
+	m.Init = function(figure)
+		SetOverrideDanceMusic(AssetGetContentId("Onenight.mp3"))
+
+		start           = os.clock()
+		animator        = AnimLib.Animator.new()
+		animator.rig    = figure
+		animator.track  = AnimLib.Track.fromfile(AssetGetPathFromFilename("nil_Chito.anim"))
+		animator.looped = true
+		animator.speed  = 1
+	end
+
+	m.Update = function(dt, figure)
+		animator:Step(os.clock() - start)
+	end
+
+	m.Destroy = function(figure)
+		animator = nil
+	end
+
+	return m
+end)
+
+return modules
